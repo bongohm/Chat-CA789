7af708af856921488aa4a74554f1c7ecb5f02b3a
bongohm <154731310+bongohm@users.noreply.github.com>
2024-02-21 20:44:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,87 +1,75 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
-const formatMessage = require("./utils/messages");
-const createAdapter = require("@socket.io/redis-adapter").createAdapter;
-const redis = require("redis");
-require("dotenv").config();
-const { createClient } = redis;
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getRoomUsers,
-} = require("./utils/users");
+const path = require('path');
+const http = require('http');
+const express  = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { 
+    userJoin, 
+    getCurrentUser, 
+    userLeave,
+    getRoomUsers
+ } = require('./utils/users');
+
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = "ChatCord Bot";
+const botname = 'ChatCord Bot';
 
-(async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
-  await pubClient.connect();
-  subClient = pubClient.duplicate();
-  io.adapter(createAdapter(pubClient, subClient));
-})();
+//Run when client connects
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room}) => {
+        const user = userJoin(socket.id, username, room);
 
-// Run when client connects
-io.on("connection", (socket) => {
-  console.log(io.of("/").adapter);
-  socket.on("joinRoom", ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
 
-    socket.join(user.room);
+        //Welcome current user
+    socket. emit('message', formatMessage(botname, 'Welcome to ChatCord!'));
 
-    // Welcome current user
-    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
-
-    // Broadcast when a user connects
+    //Broadcast when a user connects
     socket.broadcast
-      .to(user.room)
-      .emit(
-        "message",
-        formatMessage(botName, `${user.username} has joined the chat`)
-      );
+    .to(user.room)
+    .emit(
+        'message', 
+        formatMessage(botname, `${user.username} has joined the chat`)
+        );
 
-    // Send users and room info
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: getRoomUsers(user.room),
+        //send user and room info
+        io.to(user.room).emit('roomUsers',{
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
     });
-  });
 
-  // Listen for chatMessage
-  socket.on("chatMessage", (msg) => {
-    const user = getCurrentUser(socket.id);
+    //Listen for chatmessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+        
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
 
-    io.to(user.room).emit("message", formatMessage(user.username, msg));
-  });
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+    
+        if(user) {
+            io.to(user.room).emit(
+                'message', 
+                formatMessage(botname,`${user.username} has left the chat`)
+                );
+                //send user and room info
+                io.to(user.room).emit('roomUsers',{
+                    room: user.room,
+                    users: getRoomUsers(user.room)
+                });
+            }
+        });
+    });
 
-  // Runs when client disconnects
-  socket.on("disconnect", () => {
-    const user = userLeave(socket.id);
-
-    if (user) {
-      io.to(user.room).emit(
-        "message",
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-
-      // Send users and room info
-      io.to(user.room).emit("roomUsers", {
-        room: user.room,
-        users: getRoomUsers(user.room),
-      });
-    }
-  });
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3001 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
